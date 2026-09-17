a680b91cb2a62f9eda19dc21f4c3c51435ec3ff5
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2026-09-17 06:12:16 +0300

    small test

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -174,7 +174,7 @@ export const SiteHeader = () => {
                 lineHeight: 1,
               }}
             >
-              Zurièee
+              Zuriè
             </Typography>
 
             <Box
